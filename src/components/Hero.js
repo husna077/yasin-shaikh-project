3c413ba2ad3c42cf9b67ecd8aa0c22febea26140
Mahdiya Husna <shaikhhusna077@gmail.com>
2025-04-28 23:54:29 +0530

    updated design and feature

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,21 @@
 import React from "react";
 import './Hero.css';
-import host from "../assets/hostimg.avif";
+import host from "../assets/host-yasin-shaikh.png";
 import clock from "./clock2.png";
 import acc from "../assets/acc.png";
+import Calender from "../assets/calendar.png";
+import Clock from "..//assets/event.png";
+import Live from "..//assets/live-streaming.png";
+import Duration from "..//assets/duration.png";
 
 const Hero = () =>{
     return(
         <div className="elements">
         <div className="hero-elements">
             <div className="hero">
+                <h2 id="hero-id-head">
+                Certified top <span id="ten-id-span">10% </span>Soft Skills Live Online Training Course
+                </h2>
             <h2 id='element'>Trusted by 1361+ Professionals, achieve next-level growth in your career & life by mastering soft skills,
             enhancing personality, and boosting earnings.</h2>
             
@@ -18,41 +25,67 @@ const Hero = () =>{
             <div className="image-wrapper" id="host">
                 <img src={host} alt="Description" id="host-image"/>
                 <div className="host-name">
-                    <h3 id="name">HOSTED BY SHAIKH YASIN</h3>
-                    <h4 id="designation">Founder and CEO, SY-BUSINESS CONSULTANCY</h4>
+                    <h3 id="name">Shaikh Yasin</h3>
+                    <h4 id="designation">Award-Winning Business Coach, Franchise Consultant, International Corporate Trainer
+                    & Motivational Speaker.</h4>
                 </div>
                 </div>
-                </div>
-            
-            <div className="image-wrapper">
-                <div className="certified-content" >
-                     <img src={clock}  alt="Description " id="clock-img"/>
-                    <p id="text-certified">Certified top 10% Soft Skills Live Online Training Course <span id="months-span">(2 Months)</span></p>
-                </div>
-                <div className="review-class1">
-                 <div className="acc-image">
-                    <img src={acc} id=""/>
-                 </div>
-                 <div className="content-review">
-                   <p id="text-review">
-                     Thanks to your guidence, 
-                    70% of our sales issues are resolved, 
-                       and I now see bussiness as a wealth generator, not just survival.I'm enjoying a stress-free life.   
-                     </p>
-                     <span id="review-name">Mr.Imran Shaikh, <span id="review-name-des">Owner</span></span>
-                     <span id="review-location">Umama Collection Solapur.     </span>
-                     </div>
                 </div>
                 
+        <div className="image-wrapper2">
+            <div className="timing">
+                <div className="timing-one">
+                <div className="class-timing">
+                    <img src={Calender} className="logos-timing"/>
+                    <div className="logos-divs">
+                    <h3 className="logos-h3">Date</h3>
+                    <p className="logos-info">From 1 April 2025</p>
+               
+
+                    </div>
+                     </div>
+                <div className="class-timing">
+                    <img src={Clock} className="logos-timing"/>
+                   <div className="logos-divs">
+                    <h3 className="logos-h3">Time</h3>
+                    <p className="logos-info">
+                        10AM To 12PM
+                    </p>
+                    </div>
+                </div>
+                </div>
+                <div className="timing-two">
+                <div className="class-timing">
+                    <img src={Live} className="logos-timing"/>
+                    <div className="logos-divs">
+                    <h3 className="logos-h3">Live Stream</h3>
+                    <p className="logos-info">
+                        EVERY SAT & SUN
+                    </p>
+                    </div>
+                </div>
+                <div className="class-timing">
+                    <img src={Duration} className="logos-timing"/>
+                    <div className="logos-divs">
+                    <h3 className="logos-h3">Duration</h3>
+                    <p className="logos-info">
+                        14 HOURS
+                    </p>
+                    </div>
                 </div>
                 </div>
                 <div className="register-class">
-                <button className="button-register">Register Now @ just 3999 Rs/- Only </button>
+                <button className="button-register" >Register Now @ just 3999 Rs/- Only </button>
                 <div className="original-price-off-div"><p id="original-price">Original Price: Rs 44,999  </p><span id="off-span">(88% OFF)</span></div>
-                <p id="seats">ONLY 9 SEAT Left</p>
-                <p id="timing-live-session">Live Session From on April 01, 2025 Saturday | 10:00 AM</p>
+                <p id="seats">ONLY FEW SEATS LEFT</p>
+                
                
             </div>
+            </div>
+        </div>
+        </div>
+                
+                
         
         
         </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,11 @@ import './Navbar.css';
 const Navbar = () => {
   return (
     <nav className="navbar">
-     <div className='navbar-text'><h1 id='head'>Certified top 10% Soft Skills Live Online Training Course</h1>
+     <div className='navbar-text'>
+      <h1 id='head'>Certified top 10% Soft Skills Live Online Training Course</h1>
+      <p id='sub-head'>
+        Trusted by Thousand Plus Professionals across all Industries
+      </p>
      </div> 
     </nav>
   );
